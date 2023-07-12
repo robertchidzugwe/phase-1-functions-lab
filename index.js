838d754c65fff025100e5d852e0937fe7ae3f6d9
robertchidzugwe <robertchidzugwe@gmail.com>
2023-07-12 15:13:20 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+const multiply = function(num1, num2){
+console.log(num1 * num2)
+
+}
+ multiply(2,31)
+
+
+  let random_num = Math.floor(Math.random() * 100) + 1;
+
+    console.log(random_num); 
+  
+    
+  let num3 = 24;
+  let num4 = 5;
+  let mod = num3 % num4;
+  console.log(mod);
+  
+  
+  let num_set = [10, 5, 20, 15];
+  let maximum = Math.max(...num_set);
+  console.log(maximum);
+
